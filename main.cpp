a424ebd3f894daf19b3d73df6c2134cd29fb2920
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-02 12:28:10 +0900

    01_02再起関数

--- main.cpp
+++ main.cpp
@@ -6,24 +6,26 @@ int Recursion(int salaryB) {
 	return salaryB * 2 - 50;
 }
 
-int Compartion(int salaryA, int salaryB,int count) {
+int Compartion(int salaryA, int salaryB,int salaryBAdd ,int count) {
+	count++;
+	printf("%dŠÔ“­‚¢‚½‚Æ‚«\n", count);
+	salaryA = Add(salaryA);
+	printf("ˆê”Ê“I‚È’À‹à‘ÌŒn‚Ìê‡‚Í%d‰~\n", salaryA);
+	salaryB = Recursion(salaryB);
+	salaryBAdd = salaryBAdd + salaryB;
+	printf("Ä‹A“I‚È’À‹à‘ÌŒn‚Ìê‡‚Í%d‰~\n", salaryBAdd);
 	if (salaryA <= salaryB) {
 		return salaryB;
 	}
-	count++;
-	printf("%dŠÔ“­‚¢‚½‚Æ‚«\n", count);
-	salaryA= Add(salaryA);
-	printf("ˆê”Ê“I‚È’À‹à‘ÌŒn‚Ìê‡‚Í%d‰~\n", salaryA);
-	salaryB = Recursion(salaryB);
-	printf("Ä‹A“I‚È’À‹à‘ÌŒn‚Ìê‡‚Í%d‰~\n", salaryB);
-	Compartion(salaryA, salaryB,count);
+	
+	Compartion(salaryA, salaryB,salaryBAdd,count);
 	
 }
 int main() {
 	int salaryA=1072;
 	int salaryB=100;
-
+	int salaryBAdd = 100;
 	int hour = 1;
-	Compartion(salaryA, salaryB, hour);
+	Compartion(salaryA, salaryB, salaryBAdd,hour);
 	return 0;
 }
