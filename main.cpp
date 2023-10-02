33cdad4c1d8a03b808c199f32fe825bb832f4106
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-02 11:56:18 +0900

    printfを関数の中で実行

--- main.cpp
+++ main.cpp
@@ -4,20 +4,34 @@ template<typename T1,typename T2>
 
 T1 Min(T1 a, T2 b) {
 	if (static_cast<T1>(a) >= static_cast<T2>(b)) {
+		printf("%f\n", static_cast<T1>(a));
 		return static_cast<T1>(a);
 	}
 	else {
+		printf("%f\n", static_cast<T1>(b));
 		return static_cast<T1>(b);
 	}
 }
 template<>
 char Min<char>(char a, char b) {
-return	printf("数字以外は代入できません\n");
+	printf("数字以外は代入できません\n");
+	return 0;	
+}
+template<typename T1>
+int Min(int a, T1 b) {
+	if (static_cast<int>(a) >= static_cast<T1>(b)) {
+		printf("%d\n", static_cast<int>(a));
+		return static_cast<int>(a);
+	}
+	else {
+		printf("%d\n", static_cast<int>(b));
+		return static_cast<int>(b);
+	}
 }
 int main() {
-	printf("%d\n", Min<int, float>(1000, 200.0f));
-	printf("%d\n", Min<char, char>(1000, 200.0f));
-	printf("%d\n", Min<int, double>(1000, 2000.0));
-	printf("%f\n", Min<double, float>(1.7, 1.6f));
+	 Min<float>(1000, 200.0f);
+	 Min<char,char>(1000, 200.0f);
+	 Min<double>(1000, 2000.0);
+	 Min<double, float>(1.7, 1.6f);
 	return 0;
 }
