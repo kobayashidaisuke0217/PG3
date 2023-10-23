8882cf172d7d3e58a4c05c6b3d0425c040ca6ea7
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-23 10:44:51 +0900

    char型のMinに数字以外での計算を使用

--- main.cpp
+++ main.cpp
@@ -13,13 +13,14 @@ T1 Min(T1 a, T2 b) {
 	}
 }
 template<>
-char Min<char>(char a, char b) {
+char Min<char>( char a,char b) {
 	printf("数字以外は代入できません\n");
 	return 0;	
 }
 int main() {
+	
 	 Min<int,float>(1000, 200.0f);
-	 Min<char,char>(1000, 200.0f);
+	 Min<char,char>('a','b');
 	 Min<int,double>(1000, 2000.0);
 	 Min<double, float>(1.7, 1.6f);
 	return 0;
