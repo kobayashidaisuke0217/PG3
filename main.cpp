33a530baa1f9621a4b08a54b33f29e31c727afb1
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-23 11:41:38 +0900

    関数名を変更

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 ﻿#include <stdio.h>
-int Add(int salaryA) {
+int AddSalary(int salaryA) {
 	return salaryA + 1072;
 }
-int Recursion(int salaryB) {
+int CalcRecursiveSalary(int salaryB) {
 	return salaryB * 2 - 50;
 }
 
-int Compartion(int salaryA, int salaryB,int salaryBAdd ,int count) {
+int CompartionSalary(int salaryA, int salaryB,int salaryBAdd ,int count) {
 	count++;
 	printf("%d時間働いたとき\n", count);
-	salaryA = Add(salaryA);
+	salaryA = AddSalary(salaryA);
 	printf("一般的な賃金体系の場合は%d円\n", salaryA);
-	salaryB = Recursion(salaryB);
+	salaryB = CalcRecursiveSalary(salaryB);
 	salaryBAdd = salaryBAdd + salaryB;
 	printf("再帰的な賃金体系の場合は%d円\n", salaryBAdd);
 	if (salaryA <= salaryB) {
 		return salaryB;
 	}
 	
-	Compartion(salaryA, salaryB,salaryBAdd,count);
+	CompartionSalary(salaryA, salaryB,salaryBAdd,count);
 	
 }
 int main() {
@@ -26,6 +26,6 @@ int main() {
 	int salaryB=100;
 	int salaryBAdd = 100;
 	int hour = 1;
-	Compartion(salaryA, salaryB, salaryBAdd,hour);
+	CompartionSalary(salaryA, salaryB, salaryBAdd,hour);
 	return 0;
 }
