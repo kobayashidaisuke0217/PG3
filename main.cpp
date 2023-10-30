78571959a7f05654240eacf677d26c5072f74e7c
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-30 10:24:12 +0900

    丁半ゲーム

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
 ﻿#include <stdio.h>
-int main() {
+#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+typedef int (*PFunc)();
 
+int RandDice() {
+	return rand() % 6 + 1;
+}
+void Answer(PFunc p, int selectAnswer) {
+	printf("正解は...?\n");
+	//Sleep(3000);
+	int answer = p();
+	int answerNum = answer % 2;
+	int selectNum = selectAnswer % 2;
+	printf("答え%d\n", answer);
+	if (selectNum == 0) {
+		printf("予想  偶数\n");
+	}
+	else {
+		printf("予想  奇数\n");
+	}
+	if (answerNum == 0) {
+		printf("答えは偶数!\n");
+		Sleep(1500);
+	}
+	else {
+		printf("答えは奇数!\n");
+		Sleep(1500);
+	}
+
+	
+	if (selectNum == answerNum) {
+		printf("正解!!!\n\n");
+	}
+	else {
+		printf("不正解!!!\n\n");
+	}
+}
+
+int main() {
+	srand((unsigned int)time(nullptr));
+
+	PFunc pfunc = &RandDice;
+	int select;
+	printf("サイコロは偶数( 0 )、奇数( 1 )どっちでしょう\n\n");
+	printf("偶数( 0 )、奇数( 1 )どちらかを入力\n");
+	printf("予想:");
+	scanf_s("%d", &select);
+
+	// 結果発表
+	Answer(pfunc, select);
 	return 0;
 }
