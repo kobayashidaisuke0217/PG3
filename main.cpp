daae0abf2dfb88a4ccffa2b0e69502d10e806a36
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-11-13 12:02:01 +0900

    float警察

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
 	Comparison<int, float> intAndFloat(9, 19.0f);
 	Comparison<int, double> intAndDouble(10, 11.0);
 	Comparison<float, float> floatAndFloat(17.0f, 21.0f);
-	Comparison<float, double> floatAndDouble(47.0f, 20.0f);
+	Comparison<float, double> floatAndDouble(47.0f, 20.0);
 	Comparison<double, double> doubleAndDouble(2.0, 82.0);
 	printf("%d\n",intAndInt.Min());
 	printf("%d\n", intAndFloat.Min());
