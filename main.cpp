7fea6c863f489fe53ab7282f70f48c46f782148b
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-11-19 23:14:51 +0900

    ラムダを実装

--- main.cpp
+++ main.cpp
@@ -2,61 +2,54 @@
 #include <windows.h>
 #include <time.h>
 #include <functional>
-typedef int (*PFunc)(int a );
-
-
-void SetTimeOut(PFunc p,int second, int selectAnswer) {
-	Sleep(second*1000);
-	p(selectAnswer);
-}
-int Answer(int selectAnswer) {
-	
-	
-	
-	std::function<int()>diceRoll = [&]() {
-		return rand() % 6 + 1;
-	};
-	int answer = diceRoll();
-	int answerNum = answer % 2;
-	int selectNum = selectAnswer % 2;
-	printf("出た目は%d\n", answer);
-	if (selectNum == 0) {
-		printf("予想  偶数\n");
-	}
-	else {
-		printf("予想  奇数\n");
-	}
-	if (answerNum == 0) {
-		printf("答えは偶数!\n");
-		Sleep(1500);
-	}
-	else {
-		printf("答えは奇数!\n");
-		Sleep(1500);
-	}
-
-	
-	if (selectNum == answerNum) {
-		printf("正解!!!\n\n");
-	}
-	else {
-		printf("不正解!!!\n\n");
-	}
-	return 0;
-}
 
 int main() {
 	srand((unsigned int)time(nullptr));
-
-	PFunc pfunc = &Answer;
-	int select;
 	
-	printf("サイコロは偶数( 0 )、奇数( 1 )どっちでしょう\n\n");
-	printf("偶数( 0 )、奇数( 1 )どちらかを入力\n");
-	printf("予想:");
-	scanf_s("%d", &select);
-	printf("正解は?\n");
-	// 結果発表
-	SetTimeOut(pfunc,3, select);
+	std::function<int()> randomDice = []() {
+			return  rand() % 6 + 1;
+	
+	};
+	std::function<void(int, int,int)> setTimeOut = [&](int function, int selectAnswer,int waitTime) {
+		printf("正解は?\n");
+		Sleep(waitTime*1000);
+
+		// 正解
+		int answerNum = function % 2;
+		// 予想
+		int selectNum = selectAnswer % 2;
+
+		printf("%d\n", function);
+		if (answerNum == 0) {
+			printf("偶数!\n");
+			Sleep(1500);
+		}
+		else if (answerNum != 0) {
+			printf("奇数!\n");
+			Sleep(1500);
+		}
+
+		// どちらも同じ数なら正解
+		if (selectNum == answerNum) {
+			printf("正解\n");
+		}
+		else {
+			printf("不正解\n");
+		}
+	};
+	// 入力した予想
+	int selectAnswer;
+
+	printf("サイコロは奇数( 2 )、偶数( 1 )どっちだ\n\n");
+	while (true) {
+		printf("奇数( 2 )、偶数( 1 ),終了(0)どちらかを入力\n");
+		printf("予想:");
+		scanf_s("%d", &selectAnswer);
+		if (selectAnswer == 0) {
+			break;
+		}
+		setTimeOut(randomDice(), selectAnswer,3);
+	}
+
 	return 0;
 }
