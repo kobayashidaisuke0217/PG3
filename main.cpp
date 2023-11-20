186435fd660b35c345d0ce6aff7fbf6abb8012b9
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-11-20 11:09:45 +0900

    SetTimeOut作成

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <windows.h>
 #include <time.h>
 #include <functional>
-
+void SetTimeOut (std::function<void(void)> function, int waittime) {
+	Sleep(waittime * 1000);
+	function();
+};
 int main() {
 	srand((unsigned int)time(nullptr));
 	
@@ -10,45 +13,50 @@ int main() {
 			return  rand() % 6 + 1;
 	
 	};
-	std::function<void(int, int,int)> setTimeOut = [&](int function, int selectAnswer,int waitTime) {
-		printf("正解は?\n");
-		Sleep(waitTime*1000);
-
-		// 正解
-		int answerNum = function % 2;
-		// 予想
-		int selectNum = selectAnswer % 2;
-
-		printf("%d\n", function);
-		if (answerNum == 0) {
-			printf("偶数!\n");
-			Sleep(1500);
-		}
-		else if (answerNum != 0) {
-			printf("奇数!\n");
-			Sleep(1500);
-		}
-
-		// どちらも同じ数なら正解
-		if (selectNum == answerNum) {
-			printf("正解\n");
-		}
-		else {
-			printf("不正解\n");
-		}
-	};
+	
+	
 	// 入力した予想
 	int selectAnswer;
-
-	printf("サイコロは奇数( 2 )、偶数( 1 )どっちだ\n\n");
+	int waitTime = 3;
+	
+    
+	printf("サイコロは偶数( 0 ),奇数( 1 )どっちだ\n\n");
 	while (true) {
-		printf("奇数( 2 )、偶数( 1 ),終了(0)どちらかを入力\n");
+		printf("偶数( 0 ),奇数( 1 ),終了(2)どちらかを入力\n");
 		printf("予想:");
 		scanf_s("%d", &selectAnswer);
-		if (selectAnswer == 0) {
+		if (selectAnswer == 2) {
 			break;
 		}
-		setTimeOut(randomDice(), selectAnswer,3);
+		printf("正解は?\n");
+		//setTimeOut(randomDice(), selectAnswer,waitTime);
+		std::function<void()> resultDiceRoll = [&]() {
+			// 正解
+			int result = randomDice();
+			int answerNum = result % 2;
+			// 予想
+			int selectNum = selectAnswer;
+
+			printf("%d\n", result);
+			if (answerNum == 0) {
+				printf("偶数!\n");
+				Sleep(1500);
+			}
+			else if (answerNum != 0) {
+				printf("奇数!\n");
+				Sleep(1500);
+			}
+
+			// どちらも同じ数なら正解
+			if (selectNum == answerNum) {
+				printf("正解\n");
+			}
+			else {
+				printf("不正解\n");
+			}
+			
+		};
+		SetTimeOut(resultDiceRoll,waitTime);
 	}
 
 	return 0;
