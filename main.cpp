4ede1810cfe223218cafa33d9cfb9286b8205b25
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-12-27 19:33:26 +0900

    5_1完了

--- main.cpp
+++ main.cpp
@@ -1,12 +1,87 @@
 #include <stdio.h>
 #include <windows.h>
 #include <time.h>
-#include <functional>
+#include <list>
 
 
 int main() {
 
-	//文字列を表示
-	printf("うわあああああああああああああああああああああああああああああああああああ");
+	std::list<const char*> station_ = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	printf("1970年-山手線\n");
+	int count = 1;
+	for (const char* station : station_) {
+		// 駅番号
+		printf("JY%d ", count);
+		// 要素
+		printf("%s\n", station);
+		count++;
+	}
+	printf("\n");
+	//西日暮里を田畑の前に追加
+	for (std::list<const char*>::iterator itr = station_.begin(); itr != station_.end(); itr++) {
+		if (*itr == "Tabata") {
+			itr = station_.insert(itr, "Nishi-Nippori");
+			break;
+		}
+	}
+
+	printf("2019年-山手線\n");
+	count = 1;
+	for (const char* station : station_) {
+		// 駅番号
+		printf("JY%d ", count);
+		// 要素
+		printf("%s\n", station);
+		count++;
+	}
+	printf("\n");
+	//高輪ゲートウェイを田町の前に追加
+	for (std::list<const char*>::iterator itr = station_.begin(); itr != station_.end(); itr++) {
+		if (*itr == "Tamachi") {
+			itr = station_.insert(itr, "Takanawa-Gateway");
+			break;
+		}
+	}
+
+	printf("2022年-山手線\n");
+	count = 1;
+	for (const char* station : station_) {
+		// 駅番号
+		printf("JY%d ", count);
+		// 要素
+		printf("%s\n", station);
+		count++;
+	}
+	printf("\n");
 	return 0;
 }
