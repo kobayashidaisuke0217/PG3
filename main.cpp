795039d06fd41c24ce3ffda3776120260e417664
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2024-02-12 05:38:20 +0900

    ０６＿０２

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
-#include <stdio.h>
-#include<chrono>
+#include <iostream>
+#include <thread>
 #include <string>
-#include<iostream>
-int main() {
-	std::string str(10000, 'a');
-	std::chrono::system_clock::time_point start, end;
-	
-	
-	start = std::chrono::system_clock::now();
-	
-	std::string result( str);
-	end = std::chrono::system_clock::now();
 
-	double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); //処理に要した時間をマイクロ秒に変換
-	std::cout << "copy" << " : " << elapsed << std::endl;
-	
-	
-	start = std::chrono::system_clock::now();
-	 result = std::move(str);
+void PrintString(const std::string& str)
+{
+    std::cout << str << std::endl;
+}
 
-	end = std::chrono::system_clock::now();
+int main()
+{
+    //スレッド1
+    std::thread t1(PrintString, "thread1");
+    t1.join();
+    //スレッド２
+    std::thread t2(PrintString, "thread2");
+    t2.join();
+    //スレッド３
+    std::thread t3(PrintString, "thread3");
+    t3.join();
 
-	 elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); //処理に要した時間をマイクロ秒に変換
-	std::cout << "std::move" << " : " << elapsed << std::endl;
-
-		return 0;
+    return 0;
 }
